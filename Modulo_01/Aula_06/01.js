cd5e66359d17af42d4090d08745007ffd880d31d
dalima-dev <dalima.dev@gmail.com>
2022-01-24 20:46:17 -0300

    0601

---
+++ Modulo_01/Aula_06/01.js
@@ -0,0 +1,23 @@
+var prompt = require('prompt-sync')();
+
+let arr = [];
+let par = [];
+let impar = [];
+
+while (arr.length != 10){
+    arr.push(prompt('Digite um número: '))
+}
+
+let i = 0;
+
+do{
+    if(arr[i] % 2 === 0){
+        par.push(arr[i]);
+    }else{
+        impar.push(arr[i]);
+    }
+    i++;
+}while(i < arr.length);
+
+console.log(par);
+console.log(impar);
