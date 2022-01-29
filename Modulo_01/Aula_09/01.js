fb32cb4ba1aeeb1abf6eaf0cbfd37a5e1ff9d1f2
dalima-dev <dalima.dev@gmail.com>
2022-01-28 22:36:06 -0300

    format

--- Modulo_01/Aula_09/01.js
+++ Modulo_01/Aula_09/01.js
@@ -1,14 +1,15 @@
-function lista(a,b){
-    let menor; 
-    let maior;
-    if(a < b){
-        menor = a;
-        maior = b;
-    }else{
-        menor = b;
-        maior = a;
-    }
-    let arr = [];
-    for (let index = 0; index < maior - menor + 1; index++) arr.push(menor + index);
-    return arr;
+function lista(a, b) {
+  let menor;
+  let maior;
+  if (a < b) {
+    menor = a;
+    maior = b;
+  } else {
+    menor = b;
+    maior = a;
+  }
+  let arr = [];
+  for (let index = 0; index < maior - menor + 1; index++)
+    arr.push(menor + index);
+  return arr;
 }
