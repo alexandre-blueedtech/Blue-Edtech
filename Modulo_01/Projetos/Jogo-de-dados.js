253f7add8905aa1aadbc78692d1315b58793a7db
dalima-dev <dalima.dev@gmail.com>
2022-02-04 19:22:58 -0300

    dados

---
+++ Modulo_01/Projetos/Jogo-de-dados.js
@@ -0,0 +1,42 @@
+const prompt = require('prompt-sync')();
+
+function jogador(nome, dado, pontuacao){
+    this.nome = nome;
+    this.dado = dado;
+    this.pontuacao = pontuacao;
+}
+
+const rodadas = +prompt("Digite o numero de rodadas: ");
+const quantidade_jogadores = +prompt("Digite o numero de jogadores: ");
+const lista = [];
+
+for (let i = 0; i < quantidade_jogadores; i++) {
+    lista.push(new jogador(prompt(`Digite o nome do jogador ${i+1}: `), 0, 0));    
+}
+
+let dados;
+let maiorDado;
+let vencedoresRodada;
+
+for (let i = 0; i < rodadas; i++) {
+    dados = [];
+    for (let j = 0; j < lista.length; j++) {
+        dados.push(Math.floor(Math.random()*6) + 1);
+        lista[j].dado = dados[j];       
+    }
+
+    maiorDado = dados.sort((a,b) => a-b)[dados.length - 1];
+
+    for (const value of lista) {
+        if(value.dado === maiorDado) value.pontuacao++;
+    }
+
+    vencedoresRodada = lista.filter(a => a.dado === maiorDado);
+}
+const maiorPontuacao = lista.sort((a,b) => a.pontuacao - b.pontuacao)[lista.length - 1].pontuacao;
+const vencedoresJogo = lista.filter(a => a.pontuacao === maiorPontuacao);
+
+console.log(`O vencedor do jogo e: `);
+for (const value of vencedoresJogo) console.log(value.nome);
+
+
